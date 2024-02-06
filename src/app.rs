58b8c020467c55d7cec4ab2c2b481099cf143660
Nico Burniske <nicoburniske@gmail.com>
2024-02-05 21:04:09 -0600

    replicate different node kind error

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,18 @@
 use crate::error_template::{AppError, ErrorTemplate};
 use leptos::*;
+use leptos_image::Image;
 use leptos_meta::*;
+use leptos_query::{QueryOptions, QueryResult, RefetchFn};
 use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+    leptos_query::provide_query_client();
+    leptos_image::provide_image_context();
 
     view! {
-
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/reproducer-node-mismatch.css"/>
 
         // sets the document title
@@ -22,14 +22,27 @@ pub fn App() -> impl IntoView {
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
-            view! {
-                <ErrorTemplate outside_errors/>
-            }
-            .into_view()
+            view! { <ErrorTemplate outside_errors/> }.into_view()
         }>
             <main>
                 <Routes>
-                    <Route path="" view=HomePage/>
+                    <Route
+                        path=""
+                        view=|| {
+                            view! {
+                                <div>
+                                    <HomePage/>
+                                    <div>
+                                        <div>
+                                            // If you comment out the footer, there's no error??
+                                            <Footer/>
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                        }
+                    />
+
                 </Routes>
             </main>
         </Router>
@@ -39,12 +52,53 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let query = use_test_query();
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <Transition fallback=|| ()>
+            {move || {
+                query
+                    .data
+                    .get()
+                    .map(|image| {
+                        view! { <Image src=image alt="Test Image" width=300 height=300 blur=true/> }
+                    })
+            }}
+
+        </Transition>
     }
+}
+
+#[component]
+pub fn Footer() -> impl IntoView {
+    view! {
+        <footer>
+            <div>
+                <div>
+                    <p>
+                        failed: SSR and CSR elements have the same hydration key but different node kinds
+                    </p>
+                </div>
+            </div>
+        </footer>
+    }
+}
+
+fn use_test_query() -> QueryResult<String, impl RefetchFn> {
+    leptos_query::use_query(
+        || (),
+        |_| async { get_test_image().await.expect("Failed to get test image") },
+        QueryOptions {
+            default_value: None,
+            stale_time: None,
+            cache_time: None,
+            refetch_interval: None,
+            resource_option: leptos_query::ResourceOption::NonBlocking,
+        },
+    )
+}
+#[server(GetTestImage, "/api")]
+pub async fn get_test_image() -> Result<String, ServerFnError> {
+    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    Ok("cute_ferris.png".into())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ async fn main() {
     use axum::Router;
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
+    use leptos_image::ImageCacheRoute;
     use reproducer_node_mismatch::app::*;
     use reproducer_node_mismatch::fileserv::file_and_error_handler;
+
+    #[derive(Clone, axum::extract::FromRef)]
+    struct AppState {
+        leptos_options: leptos::LeptosOptions,
+        optimizer: leptos_image::ImageOptimizer,
+    }
 
     // Setting get_configuration(None) means we'll be using cargo-leptos's env values
     // For deployment these variables are:
@@ -14,14 +21,32 @@ async fn main() {
     // The file would need to be included with the executable when moved to deployment
     let conf = get_configuration(None).await.unwrap();
     let leptos_options = conf.leptos_options;
+
+    let state = AppState {
+        leptos_options: leptos_options.clone(),
+        optimizer: leptos_image::ImageOptimizer::new(
+            "/__cache/image",
+            leptos_options.site_root.clone(),
+            1,
+        ),
+    };
+
     let addr = leptos_options.site_addr;
+
+    leptos_query::suppress_query_load(true);
     let routes = generate_route_list(App);
+    leptos_query::suppress_query_load(false);
 
     // build our application with a route
     let app = Router::new()
-        .leptos_routes(&leptos_options, routes, App)
+        .route(
+            "/api/*fn_name",
+            axum::routing::post(leptos_axum::handle_server_fns),
+        )
+        .image_cache_route(&state)
+        .leptos_routes_with_context(&state, routes, state.optimizer.provide_context(), App)
         .fallback(file_and_error_handler)
-        .with_state(leptos_options);
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     logging::log!("listening on http://{}", &addr);
